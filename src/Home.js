22845c9545191f803089deddf2aa562475837852
Sakthi <87663226+Sakthiiii@users.noreply.github.com>
2021-12-26 19:43:43 +0530

    first msg

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import Hall from "./4f0a0372_15_257521-1568714218.jpeg";
+
+export default function Home() {
+  return (
+    <>
+      <h3>Welcome!</h3>
+      <img style={{ margin: "auto" }} src={Hall} />
+    </>
+  );
+}
